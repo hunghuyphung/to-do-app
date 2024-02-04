7364afd4880d1633e29ff6abc3b3aa0af35a2a0a
hungph3 <hungph3@vnpay.vn>
2024-02-04 17:17:49 +0700

    Delete task

--- src/components/ToDoItem/ToDoItem.js
+++ src/components/ToDoItem/ToDoItem.js
@@ -1,7 +1,7 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPenToSquare, faTrash} from "@fortawesome/free-solid-svg-icons";
 
-function TodoItem({task, created, completed}) {
+function TodoItem({id, task, created, completed, onDelete, onCompleted}) {
     return (
         <div className="margin-8-0 padding-8-16 bc-white border-radius-4 flex-container flex-space-between">
             <div className="flex-container">
@@ -9,6 +9,7 @@ function TodoItem({task, created, completed}) {
                     type="checkbox"
                     checked={completed}
                     className="margin-right-8"
+                    onChange={() => onCompleted(id)}
                 />
                 <div>
                     <b className="color-gray">{task}</b>
@@ -18,7 +19,11 @@ function TodoItem({task, created, completed}) {
             </div>
             <div className="color-gray">
                 <span className="margin-right-8 padding-8 bc-light-gray hover-gray">
-                    <FontAwesomeIcon icon={faTrash} style={{marginTop: "14px"}}/>
+                    <FontAwesomeIcon
+                        icon={faTrash}
+                        style={{marginTop: "14px"}}
+                        onClick={() => onDelete(id)}
+                    />
                 </span>
                 <span className="padding-8 bc-light-gray hover-gray">
                     <FontAwesomeIcon icon={faPenToSquare} style={{marginTop: "14px"}}/>
--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,35 +1,75 @@
 import TodoItem from "../ToDoItem/ToDoItem";
-
-const tasks = [
-    {
-        task: 'Create a new task',
-        created: '01/01/2024',
-        completed: true
-    }
-];
+import {useState} from "react";
 
 function TodoList() {
+    const [tasks, setTasks] = useState([]);
+    const [task, setTask] = useState('');
+    const [id, setId] = useState(0);
+
+    function handleSubmitTask() {
+        const created = (new Date()).toLocaleDateString('en-GB');
+        const completed = false;
+        const summitedTask = {
+            id,
+            task,
+            created,
+            completed
+        };
+
+        setId(prevId => prevId + 1);
+        setTasks(prevTasks => [summitedTask, ...prevTasks]);
+    }
+
+    const handleDeleteTaskById = (id) => {
+        const updatedTasks = tasks.filter(task => task.id !== id);
+        setTasks(updatedTasks);
+    };
+
+    const handleCompleteTaskById = (id) => {
+        const updatedTasks = tasks.map(task => {
+            task.completed = task.id === id ? !task.completed : task.completed;
+            return task;
+        })
+
+        setTasks(updatedTasks);
+    };
+
     return (
         <div className="margin-auto" style={{maxWidth: '600px', paddingTop: '100px'}}>
             <h1 className="text-center h-font font-42 font-weight-700 color-light-black">TODO LIST</h1>
             <br/>
             <div className="flex-container flex-space-between">
-                <button className="h-button bc-blue color-white border-radius-4 hover-gray">Add Task</button>
-                <div className="padding-0-4"></div>
-                <input type="text" placeholder="Enter task that you want to do!" className="h-input border-radius-4"/>
+                <input
+                    type="text"
+                    placeholder="Enter task that you want to do!"
+                    className="h-input margin-right-8 border-radius-4"
+                    onChange={e => setTask(e.target.value)}
+                />
+                <button
+                    className="h-button bc-blue color-white border-radius-4 hover-gray"
+                    onClick={handleSubmitTask}
+                >
+                    Add Task
+                </button>
             </div>
             <br/>
-            <div className="padding-8-16 bc-blue border-radius-4">
-                {tasks.map(task =>
-                    <TodoItem
-                        task={task.task}
-                        created={task.created}
-                        completed={task.completed}
-                    >
+            {tasks.length !== 0 && (
+                <div className="padding-8-16 bc-blue border-radius-4">
+                    {tasks.map(task =>
+                        <TodoItem
+                            key={task.id}
+                            id={task.id}
+                            task={task.task}
+                            created={task.created}
+                            completed={task.completed}
+                            onDelete={() => handleDeleteTaskById(task.id)}
+                            onCompleted={() => handleCompleteTaskById(task.id)}
+                        >
 
-                    </TodoItem>
-                )}
-            </div>
+                        </TodoItem>
+                    )}
+                </div>
+            )}
         </div>
     );
 }
